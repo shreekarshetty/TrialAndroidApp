2e6e4e3d7d62224be8559907828d4e3719d9e1cb
Shreekar Shetty <shreekar.shetty@gmail.com>
2018-10-14 17:41:58 +0100

    Added FAB

--- app/src/main/java/apps/thenerdthatwasnt/me/trialandroidapp/MainActivity.kt
+++ app/src/main/java/apps/thenerdthatwasnt/me/trialandroidapp/MainActivity.kt
@@ -1,9 +1,10 @@
 package apps.thenerdthatwasnt.me.trialandroidapp
 
-import android.app.Activity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
-class MainActivity : Activity() {
+
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
