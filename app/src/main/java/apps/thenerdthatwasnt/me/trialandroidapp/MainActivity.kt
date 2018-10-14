cee184c7d52bbe44c6e0f0b5005cd762d2db13a4
Shreekar Shetty <shreekar.shetty@gmail.com>
2018-10-14 11:01:58 +0100

    First Commit

---
+++ app/src/main/java/apps/thenerdthatwasnt/me/trialandroidapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package apps.thenerdthatwasnt.me.trialandroidapp
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
